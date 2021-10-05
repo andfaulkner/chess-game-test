378ea1b5c7db415f47592fbdb17f453e6b7bfc14
Andrew Faulkner <andfaulkner@gmail.com>
2021-10-04 22:37:20 -0400

    Submit move working (for non-capture , non-king moves)

--- app/client/components/board/board-container.jsx
+++ app/client/components/board/board-container.jsx
@@ -27,6 +27,10 @@ export class BoardContainer extends React.Component {
             [0, 2, 0, 2, 0, 2, 0, 2],
             [2, 0, 2, 0, 2, 0, 2, 0],
         ],
+        /**
+         * @type {'red'|'blue'}
+         */
+        currentPlayer: 'red',
     };
 
     /**
@@ -62,6 +66,7 @@ export class BoardContainer extends React.Component {
      */
     clickCell = (row, col) => {
         console.log('ran clickCell with row:', row, 'col:', col);
+
     };
 
     /**
@@ -72,6 +77,97 @@ export class BoardContainer extends React.Component {
      */
     submitMove = (moveValues) => {
         console.log("submitMove :: moveValues:", moveValues);
+
+        const isRed = this.state.currentPlayer === 'red';
+        const isBlue = !isRed;
+
+        const fromX = moveValues.from.x - 1;
+        const fromY = moveValues.from.y - 1;
+        const toX = moveValues.to.x - 1;
+        const toY = moveValues.to.y - 1;
+
+        const piecePositions = this.state.piecePositions;
+
+        // Correct up to here
+
+        // Selection failure conditions
+
+        // Get content of selected "from" location (i.e. whether the piece is 1 or 2 i.e. red or blue)
+        // NOTE: change when adding kings
+        const selCellContent = piecePositions[fromY][fromX];
+        console.log("selCellContent:", selCellContent);
+
+        // If true, a valid piece is selected based on the current player and cell content
+        const isValidPieceSelected = isRed ? selCellContent === 2 : selCellContent === 1;
+        console.log("isValidPieceSelected:", isValidPieceSelected);
+
+        // Define allowed move position here (basic - with basic piece).
+        /** @type [number, number][] newCoordsAllowed */
+        let newCoordsAllowed = [];
+        if (isRed) {
+            newCoordsAllowed = [[fromX - 1, fromY - 1], [fromX + 1, fromY - 1]].filter(coord => {
+                if (coord[0] < 0 || coord[0] >= 8) return false;
+                if (coord[1] < 0 || coord[1] >= 8) return false;
+                return true;
+            })
+        } else if (isBlue) {
+            // TODO Confirm this is correct
+            newCoordsAllowed = [[fromX - 1, fromY + 1], [fromX + 1, fromY + 1]].filter(coord => {
+                if (coord[0] < 0 || coord[0] >= 8) return false;
+                if (coord[1] < 0 || coord[1] >= 8) return false;
+                return true;
+            })
+        }
+
+        console.log("newCoordsAllowed:", newCoordsAllowed);
+
+        // If the requested location for the piece to move to is allowed by the rules of 
+        // checkers.
+        const isMoveValid = newCoordsAllowed.some(coord => {
+            if (coord[0] === toX && coord[1] === toY) return true;
+        });
+        console.log("isMoveValid:", isMoveValid);
+
+        // Check if the requested move location is allowed:
+        //    1. ! Check that there's a piece present of the right colour. [isValidPieceSelected]
+        //    2. ! Check the "to" location is valid. [isMoveValid]
+        //
+        // If allowed:
+        //      ! Update coordinates accordingly
+        // If not allowed:
+        //      Reject move (TODO figure out details after)
+
+        // Check if allowed, erase original piece, then draw at new location
+        if (isValidPieceSelected && isMoveValid) {
+            piecePositions[fromY][fromX] = 0;
+            piecePositions[toY][toX] = isRed ? 2 : 1;
+        }
+        
+        // Invalid move handling
+        else {
+            // Make this reason specific
+            if (!isValidPieceSelected) {
+                alert(`You must select a cell containing one of your own pieces`);
+            }
+            else if (!isMoveValid) {
+                // [0, 1], [3, 4], [5, 2]
+                const allowedMoves = newCoordsAllowed.reduce((acc, coord, idx) => {
+                    acc = acc +
+                        ((idx !== 0) ? ', ' : '') +
+                        `(${coord[0] + 1},${coord[1] + 1})`;
+                    return acc;
+                }, ``);
+                alert(`Invalid move. Available options: ${allowedMoves}`);
+            }
+            return;
+        }
+        
+        // If the rest of submitMove is successful, swap player
+        // 
+        this.setState({
+            currentPlayer: isRed ? 'blue' : 'red',
+            piecePositions: piecePositions,
+        });
     };
 
     render() {
@@ -82,6 +178,7 @@ export class BoardContainer extends React.Component {
                 swapSides={this.swapSides}
                 clickCell={this.clickCell}
                 submitMove={this.submitMove}
+                currentPlayer={this.state.currentPlayer}
             />
         );
     }
