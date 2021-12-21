fdb0b5f7de4f317071d9506fc47fccc032aca137
Andrew Faulkner <andfaulkner@gmail.com>
2021-12-20 23:41:26 -0500

    Add Typescript utility/global typings

---
+++ typings/allow-static-asset-imports.d.ts
@@ -0,0 +1,69 @@
+declare module '*.css' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.scss' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.svg' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.gif' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.bmp' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.jpg' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.jpeg' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.ico' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.woff' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.woff2' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.eot' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.ttf' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.otf' {
+    const value: any;
+    export default value;
+}
---
+++ typings/ts-utility-types.d.ts
@@ -0,0 +1,28 @@
+/**
+ * React-specific methods & properties
+ */
+declare type ReactComponentMethodsProperties =
+    | 'render'
+    | 'componentWillMount'
+    | 'componentDidMount'
+    | 'forceUpdate'
+    | 'props'
+    | 'state'
+    | 'setState'
+    | 'refs'
+    | 'context'
+    | 'shouldComponentUpdate'
+    | 'componentWillReceiveProps'
+    | 'componentWillUpdate'
+    | 'componentWillUnmount'
+    | 'componentDidCatch'
+    | 'componentDidUpdate'
+    | 'getSnapshotBeforeUpdate'
+    | 'UNSAFE_componentWillMount'
+    | 'UNSAFE_componentWillReceiveProps'
+    | 'UNSAFE_componentWillUpdate';
+
+/**
+ * Get type of Class, omitting react-specific props
+ */
+declare type OmitReactProperties<T, O = 'render'> = Omit<T, ReactComponentMethodsProperties | O>;
