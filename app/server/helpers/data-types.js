87317e7a9bb4e15fd9260232f9793ad6df9bc1eb
Andrew Faulkner <andfaulkner@gmail.com>
2022-02-01 01:02:58 -0500

    Expand data-types doc

--- app/server/helpers/data-types.js
+++ app/server/helpers/data-types.js
@@ -2,7 +2,7 @@
 const bob = 42;
 console.log(bob);
 
-// This will fail
+// This would fail
 // bob = 2222;
 // console.log(bob);
 
@@ -16,10 +16,17 @@ console.log(jon);
 const steve = `yo`;
 console.log(steve);
 
+const age = 22;
+
+// Template strings
+const stevePlusAge = `${steve} is ${age}`;
+console.log(stevePlusAge);
+
 //-------- Object data type --------
 const meeka = {
     name: 'meeka the dog',
-    age: 10
+    age: 10,
+    breed: 'Rottweiler',
 };
 console.log(meeka);
 console.log(meeka.age);
@@ -47,7 +54,12 @@ console.log(Object.values(nissan));
 console.log(Object.entries(nissan));
 
 //-------- Array data type --------
-const fruit = ['apple', 'orange', 'pear'];
+const fruit = [
+    'apple',
+    'orange',
+    'pear'
+];
+
 console.log(fruit);
 
 console.log(fruit[0]);
@@ -55,7 +67,10 @@ console.log(fruit[0]);
 const magicNumbers = [22, 77, 44, 14, 25, 97, 6010];
 console.log(magicNumbers);
 
-const cars = [{brand: 'Dodge', model: 'Charger', year: 2020, color: 'black'}, nissan];
+const cars = [
+    {brand: 'Dodge', model: 'Charger', year: 2020, color: 'black'},
+    nissan
+];
 
 console.log(cars);
 console.log(cars.length);
@@ -65,6 +80,41 @@ console.log(cars);
 
 cars.push(22);
 console.log(cars);
+
+// Joining arrays
+const normalFruit = ['apple', 'orange', 'pear'];
+const weirdFruit = ['starfruit', 'lychee', 'dragonfruit'];
+
+const allFruit = [...normalFruit, ...weirdFruit];
+console.log(allFruit);
+
+/*------------------------------------------- MATRICES -------------------------------------------*/
+// They don't really exist in Javascript - they're just nested arrays.
+
+// A real-life "matrix:"
+//
+// Grocery list:
+//      Fruit:
+//          - Orange
+//          - Apple
+//          - Pear
+//      Meat:
+//          - Chicken
+//          - Steak
+//          - Hamburger
+//      Snacks:
+//          - Chips
+//          - Cookies
+//          - Bits & Bytes
+
+const matrix = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+console.log(matrix);
+console.log(matrix[2][1]);
+
 
 //-------- Function data type --------
 // Function with no arguments
@@ -76,7 +126,7 @@ const logBasicShit = () => {
 logBasicShit();
 
 // Function with 1 argument
-const logName = nameArg => {
+const logName = (nameArg) => {
     console.log(`Your name is ${nameArg}`);
 };
 
@@ -91,6 +141,10 @@ const addNumbers = (num1, num2) => {
 const total = addNumbers(12, 50);
 console.log(total);
 
+// JS functions are dynamically typed, so this is also valid, and works.
+const text = addNumbers('hello', ' world');
+console.log(text);
+
 // Factory function
 const buildFullName = (firstName, lastName) => {
     const fullName = {firstName: firstName, lastName: lastName};
@@ -100,7 +154,10 @@ const buildFullName = (firstName, lastName) => {
 const someName = buildFullName('John', 'Doe');
 console.log(someName);
 
-// Factory function - makes mutable object
+someName.firstName = 'Jack';
+console.log(someName);
+
+// Factory function - makes mutable object with functions
 const buildPerson = (firstName, lastName, initialAge) => {
     const personsData = {age: initialAge};
 
@@ -130,6 +187,55 @@ janeNotdoe.haveBirthday();
 janeNotdoe.haveBirthday();
 janeNotdoe.haveBirthday();
 console.log(janeNotdoe.getAge());
+
+/*-------------------------- FIRST-CLASS FUNCTIONS ---------------------------*/
+// Basic first-class function example
+const runner = (cb) => {
+    // cb is: () => {
+    //    console.log('YOOOOOOOOO!!!!');
+    // }
+    // ...in the below example
+    cb();
+};
+
+runner(() => {
+    console.log('YOOOOOOOOO!!!!');
+});
+
+// ---------- Another first-class function example ---------
+const run3Times = (cb) => {
+    // cb is: () => {
+    //    console.log('Doesn't matter');
+    // }
+    // ...in the below example
+    cb();
+    cb();
+    cb();
+};
+
+run3Times(() => {
+    console.log(`Doesn't matter`);
+});
+
+run3Times(() => {
+    console.log(`qwerty`);
+    console.log(`uiop`);
+});
+
+const textConcat = (text1, text2, callback) => {
+    const fullText = `${text1}${text2}`;
+    callback(fullText);
+};
+
+textConcat('Hello ', 'world again', (someRandomText) => {
+    console.log(someRandomText);
+});
+
+const logger = (stringToLog) => {
+    console.log(`[LOG] ` + stringToLog);
+};
+
+textConcat('Hello ', 'world yet again', logger);
 
 //-------- null data type --------
 let nullValue = null;
